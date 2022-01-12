39c7d5fa6a929b57ac35ccdfaf2d5cbf8031489b
iwona <iwo.jarek@gmail.com>
2022-01-12 02:38:20 +0100

    init Accordion

--- src/js/script.js
+++ src/js/script.js
@@ -60,11 +60,11 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
-
+      thisProduct.initAccordion();
+      
       console.log('new Product:', thisProduct);
     }
-  
-      
+        
     renderInMenu(){
       const thisProduct = this;
       /* generate HTML based on template */
@@ -75,13 +75,34 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       /* add elemnt to menu */ 
       menuContainer.appendChild(thisProduct.element);
-     
+    }
+  
 
+    initAccordion(){
+      const thisProduct = this;
 
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
-      
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+
+        if(thisProduct.element != null && activeProduct != thisProduct.element){ 
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        console.log('active product:', activeProduct);
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
   }
+
+
 
   const app = {
     initMenu: function(){
