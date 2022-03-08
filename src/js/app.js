bbaac013bc5c74a204154b3ca797af919c498431
iwona <iwo.jarek@gmail.com>
2022-03-08 19:22:32 +0100

    activatePage

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
@@ -8,9 +8,28 @@ const app = {
     const thisApp = this;
 
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
     thisApp.activatePage(thisApp.pages[0].id);
+    
   },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    /* add class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;  
     for(let productData in thisApp.data.products){
@@ -21,7 +40,7 @@ const app = {
   initData: function(){
     const thisApp = this;
     thisApp.data = {};
-    const url = settings.db.url + '/' + settings.db.products;
+    const url = settings.db.url + '/' + settings.db.product;
     fetch(url)
       .then(function(rawResponse){
         return rawResponse.json();
@@ -41,6 +60,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    
   },
   initCart: function(){
     const thisApp = this;
