64853a25feeca79418c6c7663e108f0a3558aa34
iwona <iwo.jarek@gmail.com>
2022-03-10 20:06:53 +0100

    date and hour picker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import {select, templates} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking{
   constructor(element){
@@ -17,16 +19,27 @@ class Booking{
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+
+    thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets(){
     const thisBooking = this;
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+   
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+
 
     thisBooking.dom.peopleAmount.addEventListener('updated', function(){
     });
     thisBooking.dom.hoursAmount.addEventListener('updated', function(){
+    });
+    thisBooking.dom.datePicker.addEventListener('updated', function(){
+    });
+    thisBooking.dom.hourPicker.addEventListener('updated', function(){
     });
   }
 }
