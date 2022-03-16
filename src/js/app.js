ce30c56b6543aacd4f1ccacca5eef0be2522a2e7
iwona <iwo.jarek@gmail.com>
2022-03-16 13:00:43 +0100

    home page in progress

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,31 @@
 
-import {settings, select, classNames} from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
 
 const app = {
-  initPages: function(){
+  initPages: function () {
     const thisApp = this;
 
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
     const idFromHash = window.location.hash.replace('#/', '');
-    
+
     let pageMatchingHash = thisApp.pages[0].id;
 
-    for(let page of thisApp.pages){
-      if(page.id == idFromHash){
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
         pageMatchingHash = page.id;
         break;
       }
     }
 
     thisApp.activatePage(pageMatchingHash);
-    
-    for(let link of thisApp.navLinks){
-      link.addEventListener('click', function(event){
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
         const clickedElement = this;
         event.preventDefault();
 
@@ -39,38 +39,38 @@ const app = {
     }
   },
 
-  activatePage: function(pageId){
+  activatePage: function (pageId) {
     const thisApp = this;
 
     /* add class "active" to matching pages, remove from non-matching */
-    for(let page of thisApp.pages){
+    for (let page of thisApp.pages) {
       page.classList.toggle(classNames.pages.active, page.id == pageId);
     }
     /* add class "active" to matching links, remove from non-matching */
-    for(let link of thisApp.navLinks){
+    for (let link of thisApp.navLinks) {
       link.classList.toggle(
-        classNames.nav.active, 
+        classNames.nav.active,
         link.getAttribute('href') == '#/' + pageId
       );
     }
   },
 
-  initMenu: function(){
-    const thisApp = this;  
-    for(let productData in thisApp.data.products){
+  initMenu: function () {
+    const thisApp = this;
+    for (let productData in thisApp.data.products) {
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }
   },
 
-  initData: function(){
+  initData: function () {
     const thisApp = this;
     thisApp.data = {};
     const url = settings.db.url + '/' + settings.db.products;
     fetch(url)
-      .then(function(rawResponse){
+      .then(function (rawResponse) {
         return rawResponse.json();
       })
-      .then(function(parsedResponse){
+      .then(function (parsedResponse) {
         //console.log('parsedResponse', parsedResponse);
         // save parsedResponse as thisApp.data.products
         thisApp.data.products = parsedResponse;
@@ -80,32 +80,35 @@ const app = {
     console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
-  
-  initCart: function(){
+  initCart: function () {
     const thisApp = this;
     const cartElem = document.querySelector(select.containerOf.cart);
     thisApp.cart = new Cart(cartElem);
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
-    thisApp.productList.addEventListener('add-to-cart', function(event){
+    thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
     });
   },
 
-  initBooking: function(){
+  initBooking: function () {
     const thisApp = this;
 
     const bookingWidget = document.querySelector(select.containerOf.booking);
     thisApp.booking = new Booking(bookingWidget);
   },
 
-  init: function(){
+  initHome: function (){
+    
+  },
+
+  init: function () {
     const thisApp = this;
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
     thisApp.initBooking();
-    
+    thisApp.initHome();
   },
 };
 
---
+++ src/js/components/Home.js
@@ -0,0 +1,25 @@
+
+class Home {
+
+    constructor(element) {
+        const thisHome = this;
+        thisHome.render();
+
+
+
+    }
+
+
+    render(element) {
+        const thisHome = this;
+        
+
+    }
+
+
+
+
+
+
+
+}
